46b06fc765fbcb2dabd3218ec9e8fbc1acbbd02d
Maksim Momcilovic <M.Momcilovic.inf20@stud.bbbaden.ch>
2023-04-07 20:10:15 +0200

    build

--- LockMaster/src/Screens/Landing/Landing.tsx
+++ LockMaster/src/Screens/Landing/Landing.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import "./Landing.css";
-import { AiFillLock } from "react-icons/ai";
-import Suggestions from "../../Components/Suggestions/Suggestions";
 import Info from "../../Components/InfoPW/Info";
 
 const Landing = () => {
